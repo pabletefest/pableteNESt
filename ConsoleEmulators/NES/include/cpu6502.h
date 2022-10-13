7057dabe370025478a9b69e46584c8e705492c52
pabletefest <stemmerpablo@gmail.com>
2022-10-13 14:57:08 +0200

    WIP

--- ConsoleEmulators/NES/include/cpu6502.h
+++ ConsoleEmulators/NES/include/cpu6502.h
@@ -1,6 +1,33 @@
 #pragma once
+//#include <cstdint>
+
+class NESBusSystem;
 
 class CPU
 {
+public:
+	CPU(NESBusSystem* nesBus);
+	~CPU() = default;
 
+	void writeData(uint16_t address, uint8_t data);
+	uint8_t readData(uint16_t address);
+
+	void reset();
+	void clock();
+
+public:
+	// Registers of 6502 core (public to examine and debug later on)
+	uint8_t A;
+	uint8_t X;
+	uint8_t Y;
+	uint16_t PC; // Program Counter
+	uint8_t SP; // Stack Pointer
+
+	enum StatusFlags
+	{
+
+	};
+
+private:
+	NESBusSystem* bus;
 };
--- ConsoleEmulators/NES/src/cpu6502.cpp
+++ ConsoleEmulators/NES/src/cpu6502.cpp
@@ -1,1 +1,24 @@
 #include "cpu6502.h"
+#include "nesBus.h"
+
+CPU::CPU(NESBusSystem* nesBus) : bus(nesBus)
+{
+}
+
+void CPU::writeData(uint16_t address, uint8_t data)
+{
+	bus->writeData(address, data);
+}
+
+uint8_t CPU::readData(uint16_t address)
+{
+	return bus->readData(address);
+}
+
+void CPU::reset()
+{
+}
+
+void CPU::clock()
+{
+}
