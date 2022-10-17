0cf3fd954c495802c16a5868b8134931870e6242
pabletefest <stemmerpablo@gmail.com>
2022-10-17 15:15:07 +0200

    Changed order, public goes first

--- ConsoleEmulators/NES/include/cpu6502.h
+++ ConsoleEmulators/NES/include/cpu6502.h
@@ -17,6 +17,29 @@ public:
 	void reset();
 	void clock();
 
+public:
+	// Registers of 6502 core (public to examine and debug later on)
+	uint8_t A;
+	uint8_t X;
+	uint8_t Y;
+	uint16_t PC; // Program Counter
+	uint8_t SP; // Stack Pointer
+	uint8_t status; // STATUS register
+
+	enum StatusFlags
+	{
+		C = (1 << 0), // Carry bit
+		Z = (1 << 1), // Zero bit
+		I = (1 << 2), // Interrupt bit
+		D = (1 << 3), // Decimal bit
+		B = (1 << 4), // Break bit
+		U = (1 << 5), // Unused bit 5, always set to 1
+		V = (1 << 6), // Overflow bit
+		N = (1 << 7)  // Negative bit
+	};
+
+	bool isLastCompleted() const;
+
 private:
 
 	// ADDRESSING MODES
@@ -42,30 +65,6 @@ private:
 	uint8_t SEC();	uint8_t SED();	uint8_t SEI();	uint8_t STA();
 	uint8_t STX();	uint8_t STY();	uint8_t TAX();	uint8_t TAY();
 	uint8_t TSX();	uint8_t TXA();	uint8_t TXS();	uint8_t TYA();
-
-
-public:
-	// Registers of 6502 core (public to examine and debug later on)
-	uint8_t A;
-	uint8_t X;
-	uint8_t Y;
-	uint16_t PC; // Program Counter
-	uint8_t SP; // Stack Pointer
-	uint8_t status; // STATUS register
-
-	enum StatusFlags
-	{
-		C = (1 << 0), // Carry bit
-		Z = (1 << 1), // Zero bit
-		I = (1 << 2), // Interrupt bit
-		D = (1 << 3), // Decimal bit
-		B = (1 << 4), // Break bit
-		U = (1 << 5), // Unused bit 5, always set to 1
-		V = (1 << 6), // Overflow bit
-		N = (1 << 7)  // Negative bit
-	};
-
-	bool isLastCompleted() const;
 
 private:
 	NESBusSystem* bus;
