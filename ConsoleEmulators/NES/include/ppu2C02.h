29a55edbfdd9e1e4db060277c3d21fd852d302d7
pabletefest <stemmerpablo@gmail.com>
2023-01-24 14:48:25 +0100

    yeeey almost done, SPRITES kind of working

--- ConsoleEmulators/NES/include/ppu2C02.h
+++ ConsoleEmulators/NES/include/ppu2C02.h
@@ -181,7 +181,7 @@ namespace nes
         uint8_t spritesAttributesLatches[8];
         uint8_t spritesXpositionCounters[8];
 
-        uint8_t spriteRenderingCounters[8];
+        uint8_t spriteRenderingCounters[8] = { 7, 7, 7, 7, 7, 7, 7, 7 };
 
         //temp
         bool isSprPixel = false;
